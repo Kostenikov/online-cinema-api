4fc38d549bf28827045733ad9b0d3cb3e6152db6
Vasyl Boliukh <vskesha@gmail.com>
2025-10-13 02:51:58 +0300

    Add user profile and token models

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -2,6 +2,18 @@ import os
 
 from database.models.base import Base
 
+from database.models.accounts import (
+    UserModel,
+    UserGroupModel,
+    UserGroupEnum,
+    ActivationTokenModel,
+    PasswordResetTokenModel,
+    RefreshTokenModel,
+    UserProfileModel,
+)
+from database.session_sqlite import reset_sqlite_database as reset_database
+from database.validators import accounts as accounts_validators
+
 environment = os.getenv("ENVIRONMENT", "developing")
 
 if environment == "testing":
