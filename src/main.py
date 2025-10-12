e4ff363ee4e8575d49481f3523085bc8a9e618a7
Oleksii Kostenikov <akotenikov@gmail.com>
2025-10-13 00:48:14 +0300

    Configuring alembic and migrations

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI(
     title="Online Cinema API",
-    description="Description of project"
+    description="Description of project",
 )
 
 api_version_prefix = "/api/v1"
