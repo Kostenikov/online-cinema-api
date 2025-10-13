dd277123027be459acdffcc2103c863d5abec92d
Vasyl Boliukh <vskesha@gmail.com>
2025-10-13 20:35:55 +0300

    Add account router into main app

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
+from routes import accounts_router
+
 app = FastAPI(
     title="Online Cinema API",
     description="Description of project",
 )
 
 api_version_prefix = "/api/v1"
+
+app.include_router(
+    accounts_router,
+    prefix=f"{api_version_prefix}/accounts",
+    tags=["accounts"],
+)
