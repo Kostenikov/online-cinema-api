3f68ec88275f13c8bbf26742002f6e7459f57be2
Dmytro Shevchuk <shevchukkdmytro@gmail.com>
2025-10-13 12:37:56 +0300

    add models to init file
    # Conflicts:
    #	src/database/__init__.py

--- src/database/models/shopping_carts.py
+++ src/database/models/shopping_carts.py
@@ -1,36 +1,29 @@
 from datetime import datetime, timezone
 
-from database import Base
-
 from sqlalchemy import (
-    ForeignKey,
     DateTime,
-    Integer, UniqueConstraint
+    ForeignKey,
+    Integer,
+    UniqueConstraint,
 )
 from sqlalchemy.orm import (
     Mapped,
     mapped_column,
-    relationship
+    relationship,
 )
+
+from database import Base
 
 
 class CartModel(Base):
     __tablename__ = "carts"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(    Integer, primary_key=True, autoincrement=True)
 
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id", ondelete="CASCADE"),
-        unique=True,
-        nullable=False
-    )
-    user: Mapped[UserModel] = relationship(
-        "UserModel", back_populates="shopping_cart"
-    )
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), unique=True, nullable=False)
+    user: Mapped[UserModel] = relationship("UserModel", back_populates="shopping_cart")
 
-    cart_items: Mapped[list["CartItemModel"]] = relationship(
-        "CartItemModel", back_populates="cart"
-    )
+    cart_items: Mapped[list["CartItemModel"]] = relationship("CartItemModel", back_populates="cart")
 
 
 class CartItemModel(Base):
@@ -41,26 +34,12 @@ class CartItemModel(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
-    cart_id: Mapped[int] = mapped_column(
-        ForeignKey("carts.id"),
-        nullable=False
-    )
-    cart: Mapped[CartModel] = relationship(
-        "CartModel",
-        back_populates="cart_items"
-    )
+    cart_id: Mapped[int] = mapped_column(ForeignKey("carts.id"), nullable=False)
+    cart: Mapped[CartModel] = relationship("CartModel", back_populates="cart_items")
 
-    movie_id: Mapped[int] = mapped_column(
-        ForeignKey("movies.id"),
-        nullable=False
-    )
-    movie: Mapped[MovieModel] = relationship(
-        "MovieModel",
-        back_populates="cart_items"
-    )
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"), nullable=False)
+    movie: Mapped[MovieModel] = relationship("MovieModel", back_populates="cart_items")
 
     added_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
-        nullable=False,
-        default=lambda: datetime.now(timezone.utc)
+        DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc)
     )
