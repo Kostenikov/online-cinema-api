2ea4554d0ee9d989cf32828c6476969af6d4627f
Vasyl Boliukh <vskesha@gmail.com>
2025-10-15 01:43:05 +0300

    Delete broken import

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -2,7 +2,6 @@ from schemas.accounts import (
     MessageResponseSchema,
     PasswordResetCompleteRequestSchema,
     PasswordResetRequestSchema,
-    ResendActivationEmailRequestSchema,
     TokenRefreshRequestSchema,
     TokenRefreshResponseSchema,
     UserActivationRequestSchema,
