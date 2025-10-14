065bc4b55be737fbf3cf35eabd57cb27874a3bad
Vasyl Boliukh <vskesha@gmail.com>
2025-10-14 21:37:46 +0300

    Fix imports in config and database init files

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,6 +1,7 @@
 from config.dependencies import (
     get_accounts_email_notificator,
     get_jwt_auth_manager,
+    get_s3_storage_client,
     get_settings,
 )
 from config.settings import BaseAppSettings
