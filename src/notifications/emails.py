02d90581d10af3863dea34cc0d15fe4855646d1a
Vasyl Boliukh <vskesha@gmail.com>
2025-10-15 17:29:14 +0300

    Configure email to send from Gmail account

--- src/notifications/emails.py
+++ src/notifications/emails.py
@@ -57,9 +57,6 @@ class EmailSender(EmailSenderInterface):
         try:
             smtp = aiosmtplib.SMTP(hostname=self._hostname, port=self._port, start_tls=self._use_tls)
             await smtp.connect()
-            #   TODO if mailhog
-            if self._use_tls:
-                await smtp.starttls()
             await smtp.login(self._email, self._password)
             await smtp.sendmail(self._email, [recipient], message.as_string())
             await smtp.quit()
