ead6e37b86d0f04f3b0305df69702b370807e180
Dmytro Shevchuk <shevchukkdmytro@gmail.com>
2025-10-15 20:11:39 +0300

    improve delete movie logic

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, Query, status
-from sqlalchemy import select
+from sqlalchemy import select, union
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import (
@@ -10,6 +10,7 @@ from database import (
     DirectorModel,
     GenreModel,
     MovieModel,
+    OrderItemModel,
     ReactionTypeEnum,
     StarModel,
     UserModel,
@@ -315,15 +316,18 @@ async def delete_movie(
     movie_id: int,
     db: AsyncSession = Depends(get_db),
 ):
-    stmt = await db.scalars(
-        select(CartItemModel.cart_id)
-        .where(CartItemModel.movie_id == movie_id)
+    stmt = union(
+        select(CartItemModel.cart_id).where(CartItemModel.movie_id == movie_id),
+        select(OrderItemModel.order_id).where(OrderItemModel.movie_id == movie_id),
     )
-    movie_in_carts = stmt.all()
-    if movie_in_carts:
+
+    result = await db.scalars(stmt)
+    movie_in_use = result.all()
+
+    if movie_in_use:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Movie you try to delete in someones cart.",
+            detail="Movie you try to delete is used in someone's cart or order.",
         )
 
     movie = await get_movie_or_404(movie_id, db)
